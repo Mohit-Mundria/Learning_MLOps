25b17afc05781100cb3964d42019c9a379b23cd8
Mohit Mundria <mundriamohit100@gmail.com>
2026-04-29 15:26:14 +0530

    Create a new commit with following message: 1. Create the Project STructure 2. Add different files to the project structure 3. Add file "Test.ipynb" to research like which model is best and do Hyperparameter tuning using the Optuna library 4. Save the best model in the model registry folder for later use

---
+++ dvc_pipeline/data_preprocessing.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+
+def fill_nan(input_train_path:str, input_test_path:str):
+    input_train_data=pd.read_csv(input_train_path)
+    input_test_data=pd.read_csv(input_test_path)
+    
+    if 
+    for i in range(7):
+        input_train_data.iloc[]
+        
